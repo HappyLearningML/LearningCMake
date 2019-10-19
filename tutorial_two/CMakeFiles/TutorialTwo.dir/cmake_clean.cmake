847fb0c0e0d13fc2917f2f2ec0e455ca0e3607a0
HappyLearningML <649037052@qq.com>
2019-10-20 00:31:43 +0800

    modify the file of cmakelists.txt

---
+++ tutorial_two/CMakeFiles/TutorialTwo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TutorialTwo.dir/MathTools.cc.o"
+  "CMakeFiles/TutorialTwo.dir/tutorial_two.cc.o"
+  "TutorialTwo"
+  "TutorialTwo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TutorialTwo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
