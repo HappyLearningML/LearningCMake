6b25407967841bc38eddd58dfa15d9952267630b
HappyLearningML <649037052@qq.com>
2019-10-19 22:57:47 +0800

    add tutorial one which is to test one cpp file

---
+++ turorial_one/CMakeFiles/Tutorual.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tutorual.dir/tutorial.cc.o"
+  "Tutorual"
+  "Tutorual.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tutorual.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
